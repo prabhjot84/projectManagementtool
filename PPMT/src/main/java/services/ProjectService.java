2c8aa667f5703b7991d10c3301c8c66dfc49b71d
psingh84 <prabhjot.singh84@t-mobile.com>
2019-06-03 16:21:42 -0700

    Services

--- PPMT/src/main/java/services/ProjectService.java
+++ PPMT/src/main/java/services/ProjectService.java
@@ -1,8 +1,19 @@
 package services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import domain.Project;
+import repositories.ProjectRepository;
 
 @Service
 public class ProjectService {
 
+	@Autowired
+	private ProjectRepository projectRepository;
+	
+	public Project saveOrUpdateProject(Project project) {
+		
+		return projectRepository.save(project);
+	}
 }
